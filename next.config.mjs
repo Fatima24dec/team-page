9fef7bb7f5f847a18c76b1cd3c2ce151cbbecc64
Fatima Al-Ibrahim <fatimaalibrahim010@gmail.com>
2026-07-11 16:37:04 +0300

    add output export to next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+};
 
 export default withNextIntl(nextConfig);
