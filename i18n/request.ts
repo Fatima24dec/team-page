80a0889a36700c460d013c1bd7dabc650f75f725
Fatima Al-Ibrahim <fatimaalibrahim010@gmail.com>
2026-07-11 17:19:01 +0300

    use static locale for export compatibility

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,9 +1,7 @@
 import { getRequestConfig } from "next-intl/server";
-import { cookies } from "next/headers";
 
 export default getRequestConfig(async () => {
-  const cookieStore = await cookies();
-  const locale = cookieStore.get("locale")?.value || "en";
+  const locale = "en";
 
   return {
     locale,
